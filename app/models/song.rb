f7bcc6c574ca546395e264c9c15fb0247690c510
Emmanuel Delmas <emmanuel.demascroissant@gmail.com>
2014-03-28 20:53:40 +0100

    songs

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,21 @@
 class Song
+  
+  SONGS = [
+    {name: "", type: "Procession", author: "", words: ""}
+    {name: "", type: "Louange d'entrée", author: "", words: ""}
+    {name: "", type: "Kyrie", author: "", words: ""}
+    {name: "", type: "Gloria", author: "", words: ""}
+    {name: "", type: "Psaume", author: "", words: ""}
+    {name: "", type: "Alleluia", author: "", words: ""}
+    {name: "", type: "Action de grâce", author: "", words: ""}
+    {name: "", type: "Invocation à l'Esprit", author: "", words: ""}
+    {name: "", type: "Prière universelle", author: "", words: ""}
+    {name: "Ne crains pas", type: "Offertoire", author: "", words: ""}
+    {name: "", type: "Sanctus", author: "", words: ""}
+    {name: "", type: "Anamnèse", author: "", words: ""}
+    {name: "", type: "Agnus", author: "", words: ""}
+    {name: "", type: "Communion", author: "", words: ""}
+    {name: "", type: "Louange finale", author: "", words: ""}
+  ]
+  
 end
